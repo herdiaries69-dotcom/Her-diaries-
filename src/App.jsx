cda9a7a74113571dde8d17a4202d18f5baa9e6e1
herdiaries69-dotcom <herdiaries69@gmail.com>
2026-03-20 02:06:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function ItsDiariesStore() {
-  return <h1>Hello World</h1>
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Her Diaries 💖</h1>
+      <p>Welcome to my store</p>
+      <button>Shop Now</button>
+    </div>
+  )
 }
 
 export default ItsDiariesStore
