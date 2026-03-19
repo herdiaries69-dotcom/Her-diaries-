068f04f0630a906333795ae2d5fce59acdf84498
herdiaries69-dotcom <herdiaries69@gmail.com>
2026-03-20 01:56:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ItsDiariesStore from "./App";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import ItsDiariesStore from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ItsDiariesStore />
   </React.StrictMode>
-);
+)
