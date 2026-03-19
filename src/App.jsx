1060b7719b3ae231ae62ddf8d404e4532211b8c6
herdiaries69-dotcom <herdiaries69@gmail.com>
2026-03-20 00:24:21 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+const Card = ({ children }) => <div style={{border:"1px solid", padding:"10px", margin:"5px"}}>{children}</div>;
+const CardContent = ({ children }) => <div>{children}</div>;
+const Button = ({ children }) => <button>{children}</button>;
